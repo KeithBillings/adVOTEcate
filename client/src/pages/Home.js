3042452cb16c942919632b5e7190d69d724c5b8d
Keith Billings <heyitsbo91@gmail.com>
2020-10-02 19:46:22 -0700

    begin styling the home cards

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,15 +1,27 @@
 import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
 import Grid from "@material-ui/core/Grid";
-import { Card, CardContent, CardMedia } from "@material-ui/core";
+import { Card, CardContent } from "@material-ui/core";
 import theme from "../components/ThemeProvider";
 import Typography from "@material-ui/core/Typography";
+import { withStyles } from '@material-ui/core/styles/withStyles';
+
+
+const useStyles = makeStyles((theme) => ({
+  card: {
+    marginTop: "10%",
+    // justifyContent: "center"
+  }
+}));
 
 function Home() {
+  const classes = useStyles();
+
   return (
-    <Grid container spacing={1} justify={"center"} direction={"column"}>
-      <Grid container item md={6}>
-        <Card className={theme.card}>
-          <CardContent className={theme.content}>
+    <Grid container direction={"column"} >
+      <Grid container item justify={"center"}>
+        <Card className={classes.card}>
+          <CardContent>
             <Typography
               className={"MuiTypography--heading"}
               variant={"h6"}
@@ -27,8 +39,8 @@ function Home() {
           </CardContent>
         </Card>
       </Grid>
-      <Grid container item md={6}>
-        <Card className={theme.card}>
+      <Grid container item justify={"center"}>
+        <Card className={classes.card}>
           <CardContent className={theme.content}>
             <Typography
               className={"MuiTypography--heading"}
