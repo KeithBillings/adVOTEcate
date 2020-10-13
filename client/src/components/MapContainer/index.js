e4d1167983fdfe8c91bab81f4897710ffaa75047
Tiana Husted <tiana.husted@gmail.com>
2020-10-12 19:04:38 -0700

    Cleaning up code

--- client/src/components/MapContainer/index.js
+++ client/src/components/MapContainer/index.js
@@ -1,33 +1,8 @@
 import React from "react";
 import { Map, GoogleApiWrapper, Marker } from "google-maps-react";
 
-const mapStyles = {
-  width: '50%',
-  height: '50%'
-};
+function MapContainer() {
+  console.log("");
+}
 
-export function MapContainer({google, dropOffLocationsByCity}) {
-  console.log("drop of locations by city from the map container component is: ", dropOffLocationsByCity);
-
-
-  dropOffLocationsByCity.map((location) => {
-    return (
-      <Map
-        google={google}
-        zoom={14}
-        style={mapStyles}
-        defaultCenter={
-          {
-            lat: location.latitude,
-            lng: location.longitude
-          }
-        }
-      >
-        <Marker/>
-      </Map>
-    );
-})};
-
-export default GoogleApiWrapper({
-  apiKey: API_KEY_HERE
-})(MapContainer);
+export default MapContainer;
